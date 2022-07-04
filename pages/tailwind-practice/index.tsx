c506cbdec10ced593f573f962865fd14db41a6ef
kazi basnet <basnetkazi2@gmail.com>
2022-07-04 14:26:16 +0545

    tailwind and chakraUI initiallization finished

---
+++ pages/tailwind-practice/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+const Tailwind = () => {
+  return (
+    <Button colorScheme="blue" className="m-3">
+      <h1 className="text-3xl font-bold underline">hello</h1>
+    </Button>
+  );
+};
+
+export default Tailwind;
